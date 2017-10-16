9f1fd7605597aacb871bc1d8660dc5e9773f838f
oleg-webdev <cibulnicoleg@gmail.com>
2017-10-16 16:44:36 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+	res.send({ hi: 'there' })
+})
+
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	// http://localhost:5000
+	console.log(`Starting: http://localhost:${port}`)
+})
